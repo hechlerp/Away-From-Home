febce0e2b56f75f0d9ff75100127bd15cfdd21b5
Peter Hechler <peter.hechler@gmail.com>
2019-01-26 16:09:02 -0500

    Add pathfinding for emails

--- Assets/Scripts/EnemyNavigation.cs
+++ Assets/Scripts/EnemyNavigation.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class EnemyNavigation : MonoBehaviour {
+    Seeker seeker;
+    int currentWaypoint;
+    float nextWaypointDistance;
+    public bool hasPathed;
+    public Path path;
+    public bool traveling;
+    public float speed;
 
     public Vector2[] navPoints;
     int currentPointIndex = 0;
     bool patrolling;
     Vector2 destination;
     bool stopped;
-    public float speed;
     public float turnspeed;
     Quaternion targetRotation;
 
@@ -17,22 +24,65 @@ public class EnemyNavigation : MonoBehaviour {
         patrolling = true;
         stopped = false;
         destination = navPoints[0];
-        speed = 1f;
-        turnspeed = 5f;
         targetRotation = transform.rotation;
-	}
+        currentWaypoint = 0;
+        nextWaypointDistance = 0;
+        hasPathed = false;
+        seeker = GetComponent<Seeker>();
+        traveling = true;
+        GetComponent<AIPath>().maxSpeed = speed;
+    }
 	
 	void Update () {
-        if (patrolling & Vector2.Distance(destination, transform.position) < 0.01) {
-            goToNextPoint();
-        } else if (!stopped) {
-            moveToPoint();
-        }
-        if (targetRotation != transform.rotation) {
-            stepRotation();
-        }
-	}
+        if (traveling) {
+            if (!hasPathed) {
+                seeker.StartPath(transform.position, new Vector3(destination.x, destination.y, 0), OnPathComplete);
+                hasPathed = true;
+            }
+            if (getCurrentPos() == destination || Vector2.Distance(getCurrentPos(), destination) < 0.1f) {
+                traveling = false;
+            }
 
+        } else if (!stopped) {
+            goToNextPoint();
+        }
+        if (path == null) {
+            // We have no path to follow yet, so don't do anything
+            return;
+        } else if (currentWaypoint > path.vectorPath.Count) {
+            return;
+        } else if (currentWaypoint == path.vectorPath.Count) {
+            currentWaypoint++;
+            return;
+        }
+        // Direction to the next waypoint
+        if (path != null) {
+            Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+            
+            dir *= speed;
+            Debug.Log(dir);
+            //transform.position = Vector3.MoveTowards(transform.position, destination, step);
+            // The commented line is equivalent to the one below, but the one that is used
+            // is slightly faster since it does not have to calculate a square root
+            //if (Vector3.Distance (transform.position,path.vectorPath[currentWaypoint]) < nextWaypointDistance) {
+            if ((transform.position - path.vectorPath[currentWaypoint]).sqrMagnitude < nextWaypointDistance * nextWaypointDistance) {
+                currentWaypoint++;
+                return;
+            }
+
+        }
+    }
+
+    Vector2 getCurrentPos() {
+        Vector3 pos = transform.position;
+        return new Vector2(pos.x, pos.y);
+    }
+
+    public void OnPathComplete(Path p) {
+        path = p;
+        currentWaypoint = 0;
+    }
+    
     public void inspectLocation(Vector2 location) {
         destination = location;
         patrolling = false;
@@ -47,6 +97,7 @@ public class EnemyNavigation : MonoBehaviour {
         destination = new Vector2(transform.position.x, transform.position.y);
         patrolling = false;
         stopped = true;
+        traveling = false;
     }
 
     void goToNextPoint () {
@@ -55,6 +106,8 @@ public class EnemyNavigation : MonoBehaviour {
             currentPointIndex = 0;
         }
         destination = navPoints[currentPointIndex];
+        traveling = true;
+        hasPathed = false;
     }
 
     void moveToPoint() {
@@ -73,6 +126,9 @@ public class EnemyNavigation : MonoBehaviour {
     void stepRotation() {
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * turnspeed);
     }
+
+
+    
 
 
 }
