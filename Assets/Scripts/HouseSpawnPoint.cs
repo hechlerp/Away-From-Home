3154e001608695e66059bb744bef81fbce8891a0
Peter Hechler <peter.hechler@gmail.com>
2019-01-26 18:49:59 -0500

    create spawn point at house

---
+++ Assets/Scripts/HouseSpawnPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseSpawnPoint : MonoBehaviour {
+    public GameObject enemy;
+    public Vector3 spawnLocation;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void spawnParent(Vector2[] navPoints) {
+        GameObject enemyParent = Instantiate(enemy);
+        enemyParent.transform.position = transform.position + spawnLocation;
+        EnemyNavigation en = enemyParent.GetComponent<EnemyNavigation>();
+        en.speed = 3f;
+        en.navPoints = navPoints;
+    }
+}
---
+++ Assets/Scripts/SpawnTimer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTimer : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        StartCoroutine(spawnFirstParent(3));
+        StartCoroutine(spawnSecondParent(6));
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator spawnFirstParent(float time) {
+        yield return new WaitForSeconds(time);
+        Vector2[] firstNavPoints = new Vector2[4];
+        firstNavPoints[0] = new Vector2(1, 0);
+        firstNavPoints[1] = new Vector2(-1, 0);
+        firstNavPoints[2] = new Vector2(-10, -10);
+        firstNavPoints[3] = new Vector2(-1, 0);
+        GameObject.Find("House").GetComponent<HouseSpawnPoint>().spawnParent(firstNavPoints);
+    }
+
+    IEnumerator spawnSecondParent(float time) {
+        yield return new WaitForSeconds(time);
+        Vector2[] secondNavPoints = new Vector2[2];
+        secondNavPoints[0] = new Vector2(5, 0);
+        secondNavPoints[1] = new Vector2(-5, 0);
+        GameObject.Find("House").GetComponent<HouseSpawnPoint>().spawnParent(secondNavPoints);
+    }
+}
