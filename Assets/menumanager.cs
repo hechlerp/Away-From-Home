0baf084189ee904151e9bd87528a339aa28b01e8
Peter Hechler <peter.hechler@gmail.com>
2019-01-27 17:04:10 -0500

    create catlogo screen

---
+++ Assets/menumanager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menumanager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void startgame() {
+        Application.LoadLevel("Main");
+    }
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKeyUp(KeyCode.Space)) {
+            startgame();
+
+        }	
+	}
+}
