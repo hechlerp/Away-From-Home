0a18bb91dd1ba6af655c28df6ffa6d5943ff0aa9
Peter Hechler <peter.hechler@gmail.com>
2019-03-02 18:55:47 -0500

    introduce foot-based colliders to gameobjects and determine sorting layers by y position

---
+++ Assets/Scripts/StaticSortingLayers.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticSortingLayers : MonoBehaviour {
+    public int sortOrderOverride = 1000000000;
+	void Start () {
+        // check to see whether a sort order was passed in (the base number is unreachable).
+        int sortOrder = sortOrderOverride == 1000000000 ? -(int)(transform.position.y * 100) : sortOrderOverride;
+        Debug.Log(sortOrder);
+        GetComponent<SpriteRenderer>().sortingOrder = sortOrder;
+    }
+}
