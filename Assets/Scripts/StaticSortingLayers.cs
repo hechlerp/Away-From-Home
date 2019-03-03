e54d146731eb11a19e79904c8ae36c68d7170975
Peter Hechler <peter.hechler@gmail.com>
2019-03-03 14:56:54 -0500

    remove debug log in StaticSortingLayers

--- Assets/Scripts/StaticSortingLayers.cs
+++ Assets/Scripts/StaticSortingLayers.cs
@@ -7,7 +7,6 @@ public class StaticSortingLayers : MonoBehaviour {
 	void Start () {
         // check to see whether a sort order was passed in (the base number is unreachable).
         int sortOrder = sortOrderOverride == 1000000000 ? -(int)(transform.position.y * 100) : sortOrderOverride;
-        Debug.Log(sortOrder);
         GetComponent<SpriteRenderer>().sortingOrder = sortOrder;
     }
 }
