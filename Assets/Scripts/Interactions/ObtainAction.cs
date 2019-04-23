f66328d19180338041045d7629026002a6b4df69
Peter Hechler <peter.hechler@gmail.com>
2019-04-23 19:00:29 -0400

    create basic carrying logic for items

---
+++ Assets/Scripts/Interactions/ObtainAction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObtainAction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Interactable interactable;
+    public GameObject player;
+    void Start()
+    {
+        interactable = GetComponentInChildren<Interactable>();
+        interactable.setAction(gatherItem);
+        interactable.setMiddlePosition(transform.position + interactable.middlePosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void gatherItem() {
+        player.GetComponent<PlayerInventory>().addToInventory(name, 1);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    Hashtable inventory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = new Hashtable();
+    }
+    
+    public void addToInventory(string name, int amt) {
+        if (inventory.ContainsKey(name)) {
+            inventory[name] = (int)inventory[name] + amt;
+        } else {
+            inventory[name] = amt;
+        }
+    }
+
+    public bool isInInventory(string name) {
+        return inventory.ContainsKey(name) & (int)inventory[name] > 0;
+    }
+
+    public void removeFromInventory(string name, string amt) {
+        if (amt == "all") {
+            inventory.Remove(name);
+        } else {
+            inventory[name] = (int)inventory[name] - int.Parse(amt);
+            if ((int)inventory[name] < 0) {
+                inventory[name] = 0;
+            }
+        }
+    }
+}
