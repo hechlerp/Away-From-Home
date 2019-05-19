7f6917d6484c936e819b89c3795ac8c189554172
Peter Hechler <peter.hechler@gmail.com>
2019-05-19 19:04:20 -0400

    fix player sprite's sort order

--- Assets/Scripts/SortingLayers.cs
+++ Assets/Scripts/SortingLayers.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SortingLayers : MonoBehaviour {
+    public bool useParent2DCollider;
     void LateUpdate() {
-        GetComponent<SpriteRenderer>().sortingOrder = -(int)(transform.position.y * 100);
+        if (useParent2DCollider) {
+            GetComponent<SpriteRenderer>().sortingOrder = -(int)(transform.parent.GetComponent<Collider2D>().bounds.center.y * 100);
+        } else {
+            GetComponent<SpriteRenderer>().sortingOrder = -(int)(transform.position.y * 100);
+        }
     }
 }
