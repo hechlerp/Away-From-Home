bcfb49a58f57428d79013b13b36498649e08698a
Peter Hechler <peter.hechler@gmail.com>
2019-05-27 18:15:46 -0400

    add menu highlighting functionality

--- Assets/menumanager.cs
+++ Assets/menumanager.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class menumanager : MonoBehaviour {
 
+    List<System.Action> actions;
+    public List<Button> buttons;
+    public Image highlighter;
+    int selectedButtonIndex;
     // Use this for initialization
     void Start() {
-
+        actions = new List<System.Action>() {
+            startgame,
+            goToLevelSelector,
+            goToControls
+        };
+        selectedButtonIndex = 0;
+        updateHighlighterPos();
     }
     public void startgame() {
         SceneManager.LoadSceneAsync("RenderingDevScene");
@@ -20,6 +31,10 @@ public class menumanager : MonoBehaviour {
     public void goToLevelSelector() {
         SceneManager.LoadSceneAsync("LevelSelector");
     }
+
+    void updateHighlighterPos() {
+        highlighter.transform.position = buttons[selectedButtonIndex].transform.position;
+    }
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyUp(KeyCode.Space)) {
@@ -27,6 +42,22 @@ public class menumanager : MonoBehaviour {
 
         } else if (Input.GetKeyUp(KeyCode.Escape)) {
             Application.Quit();
+        } else if (Input.GetKeyUp(KeyCode.DownArrow) | Input.GetKeyUp(KeyCode.S)) {
+            if (selectedButtonIndex == 2) {
+                selectedButtonIndex = 0;
+            } else {
+                selectedButtonIndex++;
+            }
+            updateHighlighterPos();
+        } else if (Input.GetKeyUp(KeyCode.UpArrow) | Input.GetKeyUp(KeyCode.W)) {
+            if (selectedButtonIndex == 0) {
+                selectedButtonIndex = 2;
+            } else {
+                selectedButtonIndex--;
+            }
+            updateHighlighterPos();
+        } else if (Input.GetKeyUp(KeyCode.Return)) {
+            actions[selectedButtonIndex]();
         }
 	} 
 }
